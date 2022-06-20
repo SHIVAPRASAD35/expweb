9ec9b8967b43ed87b6ddad6438489cc0c322114f
G.SHIVA PRASAD REDDY <73631196+SHIVAPRASAD35@users.noreply.github.com>
2022-06-20 20:39:43 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
      
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets')
-    eleventyConfig.addPassthroughCopy('./src/admin')
+    eleventyConfig.addPassthroughCopy('./src/admin/config.yml')
     
     const { DateTime } = require("luxon");
     eleventyConfig.addFilter("postDate", (dateObj) => {
